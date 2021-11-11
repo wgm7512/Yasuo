ea025bd8c92256bf5c0a64c9e08e5393fec21719
ThroughTheNight <jmethods@163.com>
2021-11-11 09:31:42 +0800

    add: lint

--- src/types/command-type.ts
+++ src/types/command-type.ts
@@ -1,10 +1,12 @@
 interface IActionType {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   (...rest: any[]): void;
 }
 
 export interface ICommand {
   command: string,
   description: string,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   options?: any[];
   action: IActionType
 }
